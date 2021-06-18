4d07fb81dae759806ef152678626b2d05307b60d
Vincent Dim <vdim@my.centennialcollege.ca>
2021-06-18 03:58:24 -0400

    added contact route and contact controller

---
+++ .history/Server/Controllers/contact_20210618034507.ts
@@ -0,0 +1,129 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+// Clothing Model Reference - db.clothing
+import Contact from '../Models/contact';
+
+// import Util Functions
+import { UserDisplayName} from '../Util';
+
+
+// Display Functions
+
+//(R)ead in CRUD
+export function DisplayContactListPage(req: Request, res: Response, next: NextFunction): void
+{
+    // db.contact.find()
+    Contact.find((err, contactCollection) =>
+    {
+        if(err)
+        {
+            console.error(err);
+            res.end(err);
+        }
+        
+        res.render('index', { title: 'Contact List', page: 'contact-list', contact: contactCollection, displayName: UserDisplayName(req)   });
+    });
+}
+
+// Display (E)dit page
+export function DisplayEditPage(req: Request, res: Response, next: NextFunction): void
+{
+    let id = req.params.id;
+
+    // pass the id to the db
+
+    // db.contact.find({"_id": id})
+
+    Contact.findById(id, {}, {}, (err, contactItemToEdit) => 
+    {
+        if(err)
+        {
+            console.error(err);
+            res.end(err);
+        }
+
+        // show the edit view
+        res.render('index', { title: 'Edit', page: 'update', contact: contactItemToEdit, displayName: UserDisplayName(req)   });
+    });
+}
+
+// Display (C)reate page
+export function DisplayAddPage(req: Request, res: Response, next: NextFunction): void
+{
+    // show the edit view
+    res.render('index', { title: 'Add', page: 'update', contact: '', displayName: UserDisplayName(req)  });
+}
+
+// Process Functions
+
+// Process (E)dit page
+export function ProcessEditPage(req: Request, res: Response, next: NextFunction): void
+{
+    let id = req.params.id;
+
+    // instantiate a new Clothing Item
+    let updatedContactItem = new Contact
+    ({
+      "_id": id,
+      "username": req.body.name,
+      "password": req.body.brand,
+      "email": req.body.category,
+      "city": req.body.colour,
+      "profession": req.body.size,
+      "age": req.body.price
+    });
+  
+    // find the contact item via db.contact.update({"_id":id}) and then update
+    Contact.updateOne({_id: id}, updatedContactItem, {}, (err) =>{
+      if(err)
+      {
+        console.error(err);
+        res.end(err);
+      }
+  
+      res.redirect('/contact-list');
+    });
+}
+
+// Process (C)reate page
+export function ProcessAddPage(req: Request, res: Response, next: NextFunction): void
+{
+  // instantiate a new Clothing
+  let newContact = new Contact
+  ({
+    "username": req.body.name,
+    "password": req.body.brand,
+    "email": req.body.category,
+    "city": req.body.colour,
+    "profession": req.body.size,
+    "age": req.body.price
+  });
+
+  // db.clothing.insert({clothing data is here...})
+  Contact.create(newContact, (err) => {
+    if(err)
+    {
+      console.error(err);
+      res.end(err);
+    }
+
+    res.redirect('/contact-list');
+  });
+}
+
+// Process (D)elete page
+export function ProcessDeletePage(req: Request, res: Response, next: NextFunction): void
+{
+    let id = req.params.id;
+
+  // db.clothing.remove({"_id: id"})
+  Contact.remove({_id: id}, (err) => {
+    if(err)
+    {
+      console.error(err);
+      res.end(err);
+    }
+
+    res.redirect('/contact-list');
+  });
+}
